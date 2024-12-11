e2411e4f90d9d797653d8a5699e46c17e008df76
TSH-19 <tomas.santos.hilario@gmail.com>
2024-12-11 18:32:45 +0000

    faz com que sigma não seja 0 pra evitar divisão por 0

--- NaiveBayes/probsDistrNormal.m
+++ NaiveBayes/probsDistrNormal.m
@@ -4,5 +4,8 @@ function [probCondicionada] = probsDistrNormal(valor, coluna, CX, classes)
     coluna_CX = cell2mat(coluna(linhas_CX));
     mu = mean(coluna_CX);
     sigma = std(coluna_CX);
+    if sigma == 0
+        sigma = 1e-6;
+    end
     probCondicionada = (1 / (sigma * sqrt(2 * pi))) * exp(-0.5 * ((valor - mu) / sigma)^2);
 end
